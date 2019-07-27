99b147fd07eece357aee45907d16fec0318b5b78
Tyler Lindow <tyler.lindow@gmail.com>
2019-07-27 08:16:37 -0700

    Use only one new array refactor

--- getAllProductsExceptIndex/getAllProductsExceptIndex.js
+++ getAllProductsExceptIndex/getAllProductsExceptIndex.js
@@ -1,9 +1,6 @@
 function getProductsOfAllIntsExceptAtIndex(intArray) {
-  const productsFromLeft = [1];
-  let counterLeft = 0;
-  const productsFromRight = [1];
-  let counterRight = intArray.length - 1;
-  let allProducts = [];
+  const allProducts = [1];
+  let productFromRight = 1;
 
   if (intArray.length <= 1) {
     throw new Error('Not enough integers');
@@ -13,14 +10,9 @@ function getProductsOfAllIntsExceptAtIndex(intArray) {
     productsFromLeft.push(productsFromLeft[i] * intArray[i])
   }
 
-  for (let i = intArray.length - 1; i > 0; i--) {
-    productsFromRight.push(productsFromRight[counterLeft] * intArray[i]);
-    counterLeft++;
-  }
-
-  for (let i = 0; i < intArray.length; i++) {
-    allProducts.push(productsFromLeft[i] * productsFromRight[counterRight]);
-    counterRight--;
+  for (let i = intArray.length - 1; i >= 0; i--) {
+    allProducts[i] *= productFromRight;
+    productFromRight *= intArray[i];
   }
 
   return allProducts;
