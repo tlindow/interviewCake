be679873730992312ec4ecf7efe34c45558f4f50
Tyler Lindow <tyler.lindow@gmail.com>
2019-07-27 22:21:13 -0700

    Check values with in-order depth first traversal

---
+++ validateBST/validateBST.js
@@ -0,0 +1,50 @@
+class BinaryTreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left  = null;
+    this.right = null;
+  }
+
+  insertLeft(value) {
+    this.left = new BinaryTreeNode(value);
+    return this.left;
+  }
+
+  insertRight(value) {
+    this.right = new BinaryTreeNode(value);
+    return this.right;
+  }
+}
+
+function isBinarySearchTree(treeRoot) {
+
+  // Determine if the tree is a valid binary search tree
+  // in order depth first search
+  const stack = [];
+  let node = treeRoot;
+  let prevVal = -Infinity;
+  // loop as long as in the stack
+  while (stack.length || node) {
+    if (node) {
+      stack.push(node);
+      node = node.left;
+    } else {
+      node = stack.pop();
+      if (prevVal > node.value) {
+        return false;
+      }
+      prevVal = node.value;
+      node = node.right;
+    }
+  }
+  // if left, push
+  // else 
+    // node = pop
+    // if prevVal > node.value
+      // return false;
+    // prevVal = node.value
+    // if right
+      // push right
+  
+  return true;
+}
