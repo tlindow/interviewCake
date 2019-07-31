4bc016b6054f1da80208f74169b20c4cc99d6bb4
Tyler Lindow <tyler.lindow@gmail.com>
2019-07-30 22:30:15 -0700

    Use Math.min and Math.max to refactor

--- getMaxProfit/getMaxProfit.js
+++ getMaxProfit/getMaxProfit.js
@@ -1,28 +1,14 @@
 function getMaxProfit(stockPrices) {
-
-  let max = -Infinity;
-  let currMax = 0;
-  let isLocalMax = false;
-  // Calculate the max profit
-  if (stockPrices.length === 1) {
-    throw new Error('Only one price');
-  } else if (stockPrices.length === 0) {
-    throw new Error('Empty price array');
+  let min = stockPrices[0];
+  let maxDiff = stockPrices[1] - min;
+  
+  if (stockPrices.length <= 1) {
+    throw new Error('Not enough stocks to evaluate');
   }
   
-  for (let i = 0; i < stockPrices.length - 1; i++) {
-    currMax = stockPrices[i + 1] - stockPrices[i];
-    if (isLocalMax && max >= 0) {
-      currMax += max;
-    }
-    
-    if (currMax > max) {
-      max = currMax;
-      isLocalMax = true;
-    } else {
-      isLocalMax = false;
-    }
+  for (let i = 1; i < stockPrices.length - 1; i++) {
+    min = Math.min(min, stockPrices[i]);
+    maxDiff = Math.max(maxDiff, stockPrices[i + 1] - min);
   }
-
-  return max;
+  return maxDiff;
 }
