fa0bad87ad65cc64273bae1e240ccc58630d40f0
Tyler Lindow <tyler.lindow@gmail.com>
2019-08-07 22:55:02 -0700

    Refactor to shorten number of lines

--- reverseChars/reverseChars.js
+++ reverseChars/reverseChars.js
@@ -1,15 +1,12 @@
 function reverse(arrayOfChars) {
-
-  // Reverse the input array of characters in place
-  let firstIdx = 0;
-  let lastIdx = arrayOfChars.length - 1;
-  
-  while (lastIdx - firstIdx >= 1) {
-    let tmp = arrayOfChars[lastIdx];
-    arrayOfChars[lastIdx] = arrayOfChars[firstIdx];
-    arrayOfChars[firstIdx] = tmp;
-    lastIdx--;
-    firstIdx++;
+  let i = 0;
+  let j = arrayOfChars.length - 1;
+  let tmp;
+  while (j - i > 0) {
+    tmp = arrayOfChars[i];
+    arrayOfChars[i] = arrayOfChars[j];
+    arrayOfChars[j] = tmp;
+    i++;
+    j--;
   }
-  return arrayOfChars;
 }
