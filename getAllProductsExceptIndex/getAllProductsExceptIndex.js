f36f96bbb0a5e9eefc9cf5ead4afbcb7093bfd97
Tyler Lindow <tyler.lindow@gmail.com>
2019-08-07 22:28:20 -0700

    Refactor to reduce number of lines

--- getAllProductsExceptIndex/getAllProductsExceptIndex.js
+++ getAllProductsExceptIndex/getAllProductsExceptIndex.js
@@ -1,19 +1,13 @@
 function getProductsOfAllIntsExceptAtIndex(intArray) {
-  const allProducts = [1];
-  let productFromRight = 1;
-
-  if (intArray.length <= 1) {
-    throw new Error('Not enough integers');
-  }
-
+  if (intArray.length <= 1) throw new Error('Not enough elements');
+  const products = [1];
+  let currProduct = 1;
   for (let i = 0; i < intArray.length - 1; i++) {
-    productsFromLeft.push(productsFromLeft[i] * intArray[i])
+    products.push(products[i] * intArray[i]);
   }
-
-  for (let i = intArray.length - 1; i >= 0; i--) {
-    allProducts[i] *= productFromRight;
-    productFromRight *= intArray[i];
+  for (let i = intArray.length - 2; i >= 0; i--) {
+    currProduct *= intArray[i + 1];
+    products[i] *= currProduct;
   }
-
-  return allProducts;
+  return products;
 }
