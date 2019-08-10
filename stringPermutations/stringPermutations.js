0abd1ab2b64a40c3ea76308ce6e812219c684b6e
Tyler Lindow <tyler.lindow@gmail.com>
2019-08-10 13:56:26 -0700

    Refactor to reduce number of lines

--- stringPermutations/stringPermutations.js
+++ stringPermutations/stringPermutations.js
@@ -1,22 +1,17 @@
 function getPermutations(string) {
-
-  // Generate all permutations of the input string
-  if (string.length <= 1) {
-    return [string];
+  const permutations = new Set([]);
+  const firstChar = string[0];
+  
+  if (string.length <= 0) {
+    return new Set([string]);
   }
   
-  let firstChar = string.slice(0, 1);
-  let restOfChars = string.slice(1);
-  let subPermutations = getPermutations(restOfChars);
-  
-  let permutations = [];
-  subPermutations.forEach(subPermutation => {
-    for (let i = 0; i <= restOfChars.length; i++) {
-      let permutation = subPermutation.slice(0, i) + 
-                        firstChar + subPermutation.slice(i);
-      permutations.push(permutation);
+  const subPerms = getPermutations(string.slice(1));
+  subPerms.forEach(subPerm => {
+    for (let i = 0; i <= subPerm.length; i++) {
+      let perm = subPerm.slice(0, i) + firstChar + subPerm.slice(i);
+      permutations.add(perm);
     }
   });
-  
   return permutations;
 }
