b771893ab76fe7fe7c384e4434ae1e870fa373a2
Tyler Lindow <tyler.lindow@gmail.com>
2019-08-10 23:18:46 -0700

    Refactor to reduce number of lines

--- unboundKnapsack/unboundKnapsack.js
+++ unboundKnapsack/unboundKnapsack.js
@@ -1,27 +1,18 @@
-function maxDuffelBagValue(cakeTypes, weightCapacity) {
-
-  // Calculate the maximum value we can carry
-  const highestVals = [0];
-  
-  for (i = 0; i < cakeTypes.length; i++) {
-    if (cakeTypes[i].weight === 0) {
-      if (cakeTypes[i].value > 0) {
+function maxDuffelBagValue(cakeTypes, maxCap) {
+  const maxVals = [0];
+  for (let i = 1; i <= maxCap; i++) {
+    maxVals[i] = maxVals[i - 1];
+    let currMaxCap = i;
+    for (let j = 0; j < cakeTypes.length; j++) {
+      let cake = cakeTypes[j];
+      if (!cake.weight && cake.value) {
         return Infinity;
-      } else if (cakeTypes[i].value < 0) {
-        return -Infinity;
+      }
+      if (cake.weight <= currMaxCap) {
+        let weightLeft = currMaxCap - cake.weight;
+        maxVals[i] = Math.max(maxVals[i], cake.value + maxVals[weightLeft]);
       }
     }
   }
-  
-  for (let i = 1; i <= weightCapacity; i++) {
-    let currentMaxValue = 0;
-    cakeTypes.forEach(cake => {
-      if (cake.weight <= i && cake.weight !== 0) {
-        currentMaxValue = Math.max(cake.value + highestVals[i - cake.weight], currentMaxValue);
-      }
-    });
-    highestVals[i] = currentMaxValue;
-  }
-
-  return highestVals[weightCapacity];
+  return maxVals[maxCap];
 }
